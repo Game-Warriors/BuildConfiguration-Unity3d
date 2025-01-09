d3413627f526ce5db2701fcd524bdbece1111c05
mahdi fadaeddini <you@example.com>
2025-01-09 12:16:39 +0330

    adding new NamedBuildTarget override for tools in BuildUtility class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+  version : 0.1.2
+  adding new NamedBuildTarget override for tools in BuildUtility class 
+
   version : 0.1.1
   Fix checking define symbols in unity initialization to replace by saved configuration
--- Editor/Core/BuildUtility.cs
+++ Editor/Core/BuildUtility.cs
@@ -1,7 +1,6 @@
-using GameWarriors.BuildConfiguration.Editor.Data;
-using System;
 using System.Text.RegularExpressions;
 using UnityEditor;
+using UnityEditor.Build;
 
 namespace GameWarriors.BuildConfiguration.Editor.Core
 {
@@ -61,6 +60,69 @@ namespace GameWarriors.BuildConfiguration.Editor.Core
             if (curretnDefines.Contains(define.ToUpper()))
                 return true;
             return false;
+        }
+
+        public static void AddScriptingDefineSymbols(NamedBuildTarget buildTarget, string symbol)
+        {
+            if (string.IsNullOrEmpty(symbol))
+            {
+                return;
+            }
+            //string currentDefines = GetScriptingDefineSymbols(targetGroup);
+            string currentDefines = PlayerSettings.GetScriptingDefineSymbols(buildTarget);
+            currentDefines = Regex.Replace(currentDefines, symbol, "");
+            PlayerSettings.SetScriptingDefineSymbols(buildTarget, $"{currentDefines};{symbol}");
+
+        }
+
+        public static void RemoveScriptingDefineSymbols(NamedBuildTarget buildTarget, string symbol)
+        {
+            //string currentDefines = GetScriptingDefineSymbols(targetGroup);
+            string currentDefines = PlayerSettings.GetScriptingDefineSymbols(buildTarget);
+            currentDefines = Regex.Replace(currentDefines, symbol, "");
+            PlayerSettings.SetScriptingDefineSymbols(buildTarget, $"{currentDefines}");
+        }
+
+        public static string CleanScriptingDefineSymbols(NamedBuildTarget buildTarget, params string[] defines)
+        {
+            string curretnDefines = PlayerSettings.GetScriptingDefineSymbols(buildTarget);
+            string oldDefines = curretnDefines;
+            int length = defines.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                string replacePattern = defines[i].ToUpper();
+                curretnDefines = Regex.Replace(curretnDefines, replacePattern, string.Empty);
+            }
+            PlayerSettings.SetScriptingDefineSymbols(buildTarget, curretnDefines);
+            return oldDefines;
+        }
+
+        public static string FindFirstScriptingDefineSymbol(NamedBuildTarget buildTarget, params string[] defines)
+        {
+            string curretnDefines = PlayerSettings.GetScriptingDefineSymbols(buildTarget);
+            int length = defines.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                string define = defines[i].ToUpper();
+                if (curretnDefines.Contains(define))
+                    return define;
+            }
+            return null;
+        }
+
+        public static bool IsContainScriptingDefineSymbol(NamedBuildTarget buildTarget, string define)
+        {
+            string curretnDefines = PlayerSettings.GetScriptingDefineSymbols(buildTarget);
+            if (curretnDefines.Contains(define.ToUpper()))
+                return true;
+            return false;
+        }
+
+        public static NamedBuildTarget GetCurrentNamedBuildTarget()
+        {
+            BuildTarget activeBuildTarget = EditorUserBuildSettings.activeBuildTarget;
+            BuildTargetGroup buildTargetGroup = BuildPipeline.GetBuildTargetGroup(activeBuildTarget);
+            return NamedBuildTarget.FromBuildTargetGroup(buildTargetGroup);
         }
     }
 }
